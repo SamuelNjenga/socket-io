03ba355186c199eaf76e57c689c3d9994bd6c43e
Samuel Njenga <snjenga704@gmail.com>
2022-02-12 09:03:56 +0300

    feat: add io.on function in index.js which will listen on the  connection event for incoming sockets

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { Server } = require("socket.io");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(3000, () => {
