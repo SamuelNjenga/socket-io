b75584184a9d840d036bad30207b2899d7015669
Samuel Njenga <snjenga704@gmail.com>
2022-02-17 14:54:57 +0300

    feat: add returnSockets and roomSockets in index.js

--- index.js
+++ index.js
@@ -36,7 +36,38 @@ io.on("connection", (socket) => {
   socket.on("message", (data) => {
     console.log(data);
   });
+  //fetch the number of currently connected clients
+  const count = io.engine.clientsCount;
+  // may or may not be similar to the count of Socket instances in the main namespace, depending on your usage
+  const count2 = io.of("/").sockets.size;
+  console.log("Connected devices are : ", count2, count);
+
+  // #socketid
+  console.log("SID", socket.id);
+  returnSockets();
+  // make all Socket instances join the "room1" room
+  io.socketsJoin("room1");
+  roomSockets();
 });
+
+// return all Socket instances of the main namespace
+const returnSockets = async () => {
+  const sockets = await io.fetchSockets();
+  for (const socket of sockets) {
+    console.log("SOCKET ID", socket.id);
+    console.log("SOCKET HANDSHAKE", socket.handshake);
+    console.log("SOCKET ROOMS", socket.rooms);
+    console.log("SOCKET DATA", socket.data);
+  }
+};
+
+// return all Socket instances in the "room1" room of the main namespace
+const roomSockets = async () => {
+  const sockets = await io.in("room1").fetchSockets();
+  for (const socket of sockets) {
+    console.log("SOCKET ID", socket.id);
+  }
+};
 
 server.listen(5000, () => {
   console.log("listening on *:5000");
